e936c0e88ad42bf25c556143691af37312df262f
Stan Angeloff <stanimir@psp-webtech.co.uk>
2013-10-16 00:21:08 +0300

    App: Scan for episodes, add basic navigation, show fanart.
    Update preview in README.

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-/*global angular */
+/*global $, angular */
 
 (function() {
   'use strict';
@@ -8,7 +8,11 @@
 
   var xml2js = require('xml2js');
 
-  var app = angular.module('MediaBrowser', []);
+  var app = angular.module('MediaBrowser', ['ui.utils']);
+
+  var $window = $(window),
+      $document = $(document),
+      $body = $(document.body);
 
   app.value('locations', ['/home/stan/Videos']);
 
@@ -20,23 +24,8 @@
     return null;
   }
 
-  function TVShow(options, location) {
-    for (var key in options) {
-      if (Object.prototype.hasOwnProperty.call(options, key)) {
-        this[key] = options[key];
-      }
-    }
-
-    this.location = location;
-
-    this.images = {
-      folder: image(location, 'folder.jpg'),
-      fanart: image(location, 'fanart.jpg')
-    };
-  }
-
-  TVShow.fromFile = function(nfo, location, resume) {
-    fs.readFile(nfo, 'utf8', function(e, contents) {
+  function parseFile(file, resume) {
+    fs.readFile(file, 'utf8', function(e, contents) {
       if (e) {
         return resume(e);
       }
@@ -50,10 +39,60 @@
         if (e) {
           return resume(e);
         }
-        resume(null, new TVShow(result, location));
+        resume(null, result);
       });
     });
+  }
+
+  function extend() {
+    var result = arguments[0] || {};
+    for (var i = 1, length = arguments.length; i < length; i = i + 1) {
+      if ( ! arguments[i]) {
+        continue;
+      }
+      for (var key in arguments[i]) {
+        if (Object.prototype.hasOwnProperty.call(arguments[i], key)) {
+          result[key] = arguments[i][key];
+        }
+      }
+    }
+    return result;
+  }
+
+  var TV = { };
+
+  TV.Show = function TVShow(options) {
+    extend(this, options);
+    extend(this, {
+      episodes: [],
+      images: {
+        folder: image(this.location, 'folder.jpg'),
+        fanart: image(this.location, 'fanart.jpg')
+      }
+    });
   };
+
+  TV.Episode = function TVEpisode(options) {
+    extend(this, options);
+  };
+
+  function fromFile(type) {
+    return function(file, options, resume) {
+      if (typeof resume === 'undefined') {
+        resume = options;
+        options = null;
+      }
+      parseFile(file, function(e, xml) {
+        if (e) {
+          return resume(e);
+        }
+        resume(null, new TV[type](extend({}, xml, options)));
+      });
+    };
+  }
+
+  TV.Show.fromFile = fromFile('Show');
+  TV.Episode.fromFile = fromFile('Episode');
 
   function scan(location, resume) {
     fs.readdir(location, function(e, files) {
@@ -66,18 +105,8 @@
           if (e) {
             return resume(e);
           }
+          resume(null, absolute, stat);
           if (stat.isDirectory()) {
-            var nfo = path.join(absolute, 'tvshow.nfo');
-            fs.exists(nfo, function(exists) {
-              if (e) {
-                return resume(e);
-              }
-              if (exists) {
-                TVShow.fromFile(nfo, absolute, function(e, show) {
-                  resume(e, show);
-                });
-              }
-            });
             scan(absolute, resume);
           }
         });
@@ -85,15 +114,100 @@
     });
   }
 
-  app.controller('VideoList', ['$scope', 'locations', function($scope, locations) {
+  function scanShows(location, resume) {
+    scan(location, function(e, file) {
+      if (e) {
+        return resume(e);
+      }
+      if (/tvshow\.nfo$/.test(file)) {
+        var location = path.dirname(file);
+        TV.Show.fromFile(file, { location: location }, function(e, tvShow) {
+          if (e) {
+            return resume(e);
+          }
+          resume(null, tvShow);
+          scan(location, function(e, file) {
+            if (e) {
+              return resume(e);
+            }
+            var basename = path.basename(file);
+            if (basename.indexOf(tvShow.title) === 0 && /\.nfo$/.test(file)) {
+              TV.Episode.fromFile(file, function(e, tvEpisode) {
+                if (e) {
+                  return resume(e);
+                }
+                tvShow.episodes.push(tvEpisode);
+                resume(null, tvEpisode);
+              });
+            }
+          });
+        });
+      }
+    });
+  }
+
+  app.controller('VideoList', ['$scope', '$timeout', 'locations', function($scope, $timeout, locations) {
+
     $scope.shows = [];
+    $scope.selectedShowIndex = 0;
+
+    var scrollQ;
+    $scope.$watch('selectedShowIndex', function() {
+      if (scrollQ) {
+        $timeout.cancel(scrollQ);
+      }
+      scrollQ = $timeout(function() {
+        scrollQ = null;
+
+        var $element = $('#shows .selected');
+        if ( ! $element.length) {
+          return false;
+        }
+
+        var viewportWidth = $window.width(),
+            viewportHeight = $window.height(),
+            elementWidth = $element.width(),
+            elementHeight = $element.height(),
+            elementOffset = $element.offset();
+
+        $body.stop().animate({
+          scrollTop: (elementOffset.top + (elementHeight / 2) - (viewportHeight / 2)),
+          scrollLeft: (elementOffset.left + (elementWidth / 2) - (viewportWidth / 2))
+        }, 175);
+
+      }, 1000 / 60, false);
+    });
+
+    $scope.selectNextShow = function($event) {
+      $event.preventDefault();
+      $scope.selectedShowIndex = Math.min($scope.selectedShowIndex + 1, $scope.shows.length - 1);
+    };
+
+    $scope.selectPreviousShow = function($event) {
+      $event.preventDefault();
+      $scope.selectedShowIndex = Math.max(0, $scope.selectedShowIndex - 1);
+    };
+
+    var digestQ;
+    function digest(timeout) {
+      if (digestQ) {
+        $timeout.cancel(digestQ);
+      }
+      digestQ = $timeout(function() {
+        digestQ = null;
+        $scope.$digest();
+      }, timeout, false);
+    }
 
     locations.forEach(function(location) {
-      scan(location, function(e, show) {
-        if (show) {
-          $scope.shows.push(show);
-          $scope.$digest();
+      scanShows(location, function(e, entity) {
+        if (e) {
+          throw e;
         }
+        if (entity instanceof TV.Show) {
+          $scope.shows.push(entity);
+        }
+        digest(125);
       });
     });
   }]);
